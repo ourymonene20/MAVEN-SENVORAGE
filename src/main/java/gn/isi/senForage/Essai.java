968cf13d85540151e494d3630cafd578ef4c1090
Mamadou Oury DIALLO <ourymonene2011@gmail.com>
2022-01-23 01:11:17 +0000

    first commit

---
+++ src/main/java/gn/isi/senForage/Essai.java
@@ -0,0 +1,23 @@
+package gn.isi.senForage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Essai {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> nom = new ArrayList<String>();
+		
+		
+		nom.add("DIALLO");
+		nom.add("OURY");
+		nom.add("LAMARANA");
+		nom.add("Claude");
+		
+		
+		nom.forEach(n-> System.out.println(n));
+
+	}
+
+}
